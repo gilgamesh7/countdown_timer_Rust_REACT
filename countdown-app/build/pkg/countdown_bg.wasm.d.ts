87e8d391a699a910f67d3b2e51e6368fe1a6a75d
Rajesh <45181827+gilgamesh7@users.noreply.github.com>
2024-11-16 03:12:19 +0000

    Build after title is set

---
+++ countdown-app/build/pkg/countdown_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function calculate_time_left(): Array;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_start(): void;
